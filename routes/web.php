e44487d613a02c5020bd2feaf61e02b80d45aa18
Tu Nombre <betopinzon1234@gmail.com>
2026-07-10 15:13:14 -0600

    Agrega módulo de escaneo de códigos de barras con html5-qrcode

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\PendientesController;
 use App\Http\Controllers\PersonalController;
 use App\Http\Controllers\ReportesController;
+use App\Http\Controllers\EscanerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'showLogin']);
@@ -74,6 +75,9 @@ Route::middleware('auth')->group(function () {
     Route::middleware('admin.only')->group(function () {
         Route::put('/pendientes/{bien}/resolver', [PendientesController::class, 'resolver'])->name('admin.pendientes.resolver');
     });
+
+    Route::get('/escanear', [EscanerController::class, 'index'])->name('admin.escanear');
+    Route::get('/escanear/{codigo}', [EscanerController::class, 'buscar'])->name('admin.escanear.buscar');
 
     Route::get('/usuarios', [ConfiguracionController::class, 'index'])->name('admin.usuarios');
     Route::middleware('admin.only')->group(function () {
